ac2c0b4e94ed455b734b83f2de445c0779275f53
saeedjalali <saeedjalalisj@gmail.com>
2022-02-01 16:51:16 +0330

    add auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { createTypeOrmProdConfig } from './app.db.config';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot(createTypeOrmProdConfig()),
     UsersModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
