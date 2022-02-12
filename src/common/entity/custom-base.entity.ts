4d18f65db77d98d2e85e27904b4db16c2b66041f
saeedjalali <saeedjalalisj@gmail.com>
2022-02-12 17:49:46 +0330

    add custom base entity

---
+++ src/common/entity/custom-base.entity.ts
@@ -0,0 +1,19 @@
+import { BaseEntity, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+
+/**
+ * custom base entity
+ */
+export abstract class CustomBaseEntity extends BaseEntity {
+  @CreateDateColumn({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  createdAt: Date;
+
+  @UpdateDateColumn({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+    onUpdate: 'CURRENT_TIMESTAMP',
+  })
+  updatedAt: Date;
+}
